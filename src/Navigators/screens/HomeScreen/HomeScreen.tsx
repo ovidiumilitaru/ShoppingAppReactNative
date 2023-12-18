34fa76a303c70d1c57deadfe9353a5ea9118d3c8
Ovidiu Militaru <ovidiu.militaru@whitbread.com>
2023-12-18 13:37:17 +0200

    added FlatList & useEffect fetch products

--- src/Navigators/screens/HomeScreen/HomeScreen.tsx
+++ src/Navigators/screens/HomeScreen/HomeScreen.tsx
@@ -1,21 +1,89 @@
-import React, { ReactNode } from 'react';
-import { Text } from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useState, useEffect } from 'react';
+import { Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
+type ItemData = {
+  id: string;
+  title: string;
+};
+
+type ItemProps = {
+  item: ItemData;
+  onPress: () => void;
+  backgroundColor: string;
+  textColor: string;
+};
+
+const DUMMYDATA: ItemData[] = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({item, onPress, backgroundColor, textColor}: ItemProps) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
+    <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
+  </TouchableOpacity>
+);
 
 export default function HomeScreen() {
+  const [selectedId, setSelectedId] = useState<string>();
+  const [data, setData] = useState<ItemData[]>([]);
+  
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(json => {
+      console.log(json.products);
+      setData([...json.products])
+    })
+    .catch(error => console.log('Have an error: ', error))
+  }, [])
+
+  const renderItem = ({item}: {item: ItemData} ) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={backgroundColor}
+        textColor={color}
+      />
+    );
+  };
+
   return (
-    <HomeScreenView>
-      <Text style={{ fontSize: 20, color: '#ffffff'}}>Home Screen styled</Text>
-      <MaterialCommunityIcons name="home" color="#ffffff" size={30} />
-    </HomeScreenView>
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      extraData={selectedId}
+    />
   )
 }
 
-const HomeScreenView = styled.View`
-  flex: 1; 
-  justify-content: center; 
-  align-items: center; 
-  background-color: #03cafc;
-`;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
