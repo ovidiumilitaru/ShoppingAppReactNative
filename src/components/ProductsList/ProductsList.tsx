bd53ff44ad1d05a9367e4268ef575c7bd90c1130
Ovidiu Militaru <ovidiu.militaru@whitbread.com>
2023-12-22 17:18:22 +0200

    filter works

---
+++ src/components/ProductsList/ProductsList.tsx
@@ -0,0 +1,78 @@
+import { Text, FlatList, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+
+// import styled from 'styled-components/native';
+
+type ItemData = {
+  id: string;
+  title: string;
+  brand: string;
+  category: string;
+};
+
+type ItemProps = {
+  item: ItemData;
+  onPress: () => void;
+  backgroundColor: string;
+  textColor: string;
+};
+
+const Item = ({item, onPress, backgroundColor, textColor}: ItemProps) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
+    <Text style={[styles.title, {color: textColor}]}>Title: {item.title}</Text>
+    <Text style={[styles.brand, {color: textColor}]}>Brand: {item.brand}</Text>
+    <Text style={[styles.category, {color: textColor}]}>Category: {item.category}</Text>
+  </TouchableOpacity>
+);
+
+
+export default function ProductsList(props: any) {
+  const [selectedId, setSelectedId] = useState<string>(); 
+
+  const { products } = props;
+
+  const renderItem = ({item}: {item: ItemData} ) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={backgroundColor}
+        textColor={color}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={products ?? []}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      extraData={selectedId}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#f9c2ff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 18,
+    paddingVertical: 4,
+  },
+  brand: {
+    fontSize: 16,
+    paddingVertical: 4,
+  },
+  category: {
+    fontSize: 14,
+    paddingVertical: 4,
+  }
+});
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import HomeScreenFilter from './HomeScreenFilter/HomeScreenFilter';
+import ProductsList from './ProductsList/ProductsList';
+
+export { HomeScreenFilter };
+export { ProductsList };
