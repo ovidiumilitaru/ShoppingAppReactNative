c0215807ccfcea29946667cc6f466c38e718dc54
Ovidiu Militaru <ovidiu.militaru@whitbread.com>
2024-01-17 16:04:04 +0200

    prod details comp added skeleton

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -26,3 +26,14 @@ export const URL = {
   baseUrl: 'https://dummyjson.com',
   products: 'products',
 }
+
+export const LOREMIPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
